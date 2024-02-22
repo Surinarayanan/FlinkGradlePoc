01d232e183d50ad0df9d4762becf00d5cbc6cee3
Suriyanarayanan Karthikeyan <52992341+Surinarayanan@users.noreply.github.com>
2024-02-22 13:56:23 +0530

    AWS kinesis

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.connector.source.Source;
 import org.apache.flink.connector.jdbc.*;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -13,6 +11,8 @@ import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Main {
@@ -20,34 +20,69 @@ public class Main {
         // Sets up the execution environment, which is the main entry point
         // to building Flink applications.
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        System.out.println("Suriya");
-        String topic="P360_AUDIT_MESSAGE";
-
-
-        Properties consumerConfig = new Properties();
-        consumerConfig.put(AWSConfigConstants.AWS_REGION, "ap-south-1");
-        consumerConfig.put(AWSConfigConstants.AWS_ACCESS_KEY_ID, "AKIA6GBLMM7VK4CCDEUF");
-        consumerConfig.put(AWSConfigConstants.AWS_SECRET_ACCESS_KEY, "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");
-        consumerConfig.put(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "LATEST");
-        consumerConfig.setProperty(ConsumerConfigConstants.SHARD_USE_ADAPTIVE_READS, "true");
-
-        FlinkKinesisConsumer<KinesisiBean> kinesisConsumer =
-                new FlinkKinesisConsumer<>("Flink-POC-ASH", new JSONStringValueDeserializationSchema(), consumerConfig);
-        //kinesisConsumer.setPeriodicWatermarkAssigner(new CustomAssignerWithPeriodicWatermarks());
-        DataStream<KinesisiBean> stream = env.addSource(kinesisConsumer);
-                //env.addSource(kinesisConsumer);
-        stream.print();
-
-        // DataStream<AuditBean> auditBeanDataStream = env.addSource(kinesisConsumer);
-        // auditBeanDataStream.print();
-       // updatePostgresqlUsingKafka(topic, env);
-
+        updatePostgresqlUsingkinesis(env);
+        updatePostgresqlUsingKafka(env);
         // Execute program, beginning computation.
         env.execute("Flink Java API Skeleton");
     }
 
-    private static void updatePostgresqlUsingKafka(String topic, StreamExecutionEnvironment env) {
+    private static void updatePostgresqlUsingkinesis(StreamExecutionEnvironment env) {
+        Properties consumerConfig = new Properties();
+        consumerConfig.put(AWSConfigConstants.AWS_REGION, "ap-south-1");
+        consumerConfig.put(AWSConfigConstants.AWS_ENDPOINT, "https://kinesis.ap-south-1.amazonaws.com");
+        consumerConfig.put(AWSConfigConstants.AWS_ACCESS_KEY_ID, "AKIA6GBLMM7VK4CCDEUF");
+        consumerConfig.put(AWSConfigConstants.AWS_SECRET_ACCESS_KEY, "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");
+        consumerConfig.put(ConsumerConfigConstants.STREAM_INITIAL_POSITION, "TRIM_HORIZON");
+        consumerConfig.setProperty(ConsumerConfigConstants.SHARD_USE_ADAPTIVE_READS, "true");
+
+
+        FlinkKinesisConsumer<KinesisiBean> kinesisConsumer = new FlinkKinesisConsumer<>(
+                "Flink-POC-ASH",
+                new JSONStringValueDeserializationSchema(),
+                consumerConfig
+        );
+        DataStream<KinesisiBean> kinesisStream = env.addSource(kinesisConsumer);
+        DataStream<KinesisiBean> parsedStream =
+            kinesisStream.map(
+                jsonString -> {
+                  // Parse your JSON data and extract relevant fields
+                  // Return a Tuple2 with key and value to be inserted into PostgreSQL
+                  System.out.println("Data "+jsonString);
+                  return jsonString;
+                });
+
+        JdbcExecutionOptions executionOptions = JdbcExecutionOptions.builder()
+                .withBatchSize(1000)
+                .withBatchIntervalMs(200)
+                .withMaxRetries(5)
+                .build();
+        JdbcConnectionOptions connectionOptions = new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                .withDriverName("org.postgresql.Driver")
+                .withUrl("jdbc:postgresql://192.168.1.230:5432/ads")
+                .withUsername("adsuser")
+                .withPassword("AdS@3421")
+                .build();
+        parsedStream
+            .addSink(
+                JdbcSink.sink(
+                    "INSERT INTO public.audit (context,category) VALUES (?,?)",
+                    new JdbcStatementBuilder<KinesisiBean>() {
+                      @Override
+                      public void accept(
+                              PreparedStatement preparedStatement, KinesisiBean record)
+                          throws SQLException {
+                          System.out.println("Data "+record);
+                          preparedStatement.setString(1, record.getKey1());
+                          preparedStatement.setString(2, record.getAnotherString());
+                      }
+                    },
+                    executionOptions,
+                    connectionOptions))
+            .name(" Insert into audit table");
+    }
+
+    private static void updatePostgresqlUsingKafka(StreamExecutionEnvironment env) {
+        String topic="P360_AUDIT_MESSAGE";
         Properties kafkaProperties = new Properties();
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
